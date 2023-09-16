cffa6dc8f394b53a8b4a19db072390e644b0c0fa
Jhonatan Augusto <jhonatanaugusto4@gmail.com>
2023-09-16 04:51:33 -0300

    dynamic table and data items display

--- src/components/IPokemonData.tsx
+++ src/components/IPokemonData.tsx
@@ -1,4 +1,15 @@
-export default interface IPokemonData{
+export interface IPokemonDataLists{
+    abilities:IAbility[]
+    forms:INamedAPIResource[]
+    game_indices:[]
+    held_items:[]
+    moves:IMove[]
+    stats:[]
+    types:[]
+    past_types:[]
+}
+
+export default interface IPokemonData extends IPokemonDataLists{
     name:string
     id:number
     base_experience:number
@@ -6,33 +17,25 @@ export default interface IPokemonData{
     is_default:boolean
     order:number
     weight:number
-    abilities:IAbility[]
-    forms:[]
-    game_indices:[]
     location_area_encounters:string
-    held_items:[]
-    moves:IMove[]
-    species:INameUrl
+    species:INamedAPIResource
     sprites:{back_default:string, back_female:string, back_shiny:string, back_shiny_female:string, front_default:string, front_female:string, front_shiny:string, front_shiny_female:string}
-    stats:[]
-    types:[]
-    past_types:[]
 }
 
-export interface INameUrl{
+export interface INamedAPIResource{
     name: string,
     url: string
 }
 
 export interface IAbility{
-    ability:INameUrl
+    ability:INamedAPIResource
     is_hidden:boolean
     slot:number
 }
 
 export interface IMove{
-    move:INameUrl
-    version_group_details:{level_learned_at:number, move_learn_method:INameUrl, version_group:INameUrl}[]
+    move:INamedAPIResource
+    version_group_details:{level_learned_at:number, move_learn_method:INamedAPIResource, version_group:INamedAPIResource}[]
 }
 
 export class PokemonData implements IPokemonData{
@@ -44,12 +47,12 @@ export class PokemonData implements IPokemonData{
     order:number
     weight:number
     abilities:IAbility[]
-    forms:[]
+    forms:INamedAPIResource[]
     game_indices:[]
     location_area_encounters:string
     held_items:[]
     moves:IMove[]
-    species:INameUrl
+    species:INamedAPIResource
     sprites:{back_default:string, back_female:string, back_shiny:string, back_shiny_female:string, front_default:string, front_female:string, front_shiny:string, front_shiny_female:string}
     stats:[]
     types:[]
--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,5 +1,35 @@
 import "./Pokemon.scss"
-import IPokemonData from "./IPokemonData"
+import IPokemonData, {IAbility, IMove, IPokemonDataLists} from "./IPokemonData"
+
+interface PokemonDataPropKey{
+    prop: keyof IPokemonData
+    title: string
+}
+
+interface PokemonDataListPropKey{
+    list: keyof IPokemonDataLists
+    object: string /*this could be any of the keys in IPokemonDataLists. How do I it in table rendering call?*/
+}
+
+const DATA_PROPS:PokemonDataPropKey[] = [
+    {prop: "name", title: "name"},
+    {prop: "base_experience", title:"base  experience"},
+    {prop: "height", title: "height"},
+    {prop: "is_default", title:"is  default?"},
+    {prop: "order", title: "order"},
+    {prop: "weight", title: "weight"},
+]
+
+const DATA_LISTS:PokemonDataListPropKey[] = [
+    {list: "abilities", object: "ability" as keyof IAbility},
+    //{list: "forms", object: ""},
+    //{list: "game_indices", object: ""},
+    //{list: "held_items", object: ""},
+    {list: "moves", object: "move" as keyof IMove},
+    //{list: "stats", object: ""},
+    //{list: "types", object: ""},
+    //{list: "past_types", object: ""},
+]
 
 interface PokemonProps{
     data: IPokemonData
@@ -7,51 +37,53 @@ interface PokemonProps{
 }
 
 export default function Pokemon({data, loading}:PokemonProps) {
+    function parseProps(prop: keyof IPokemonData):string {
+        if(typeof data[prop] === "string") return ""+data[prop]
+        if(typeof data[prop] === "boolean"){
+            if(data[prop]) return "yes"
+            else return "no"
+        } 
+        return JSON.stringify(data[prop])
+    }
+    
     if (!data.id) return "Please click on a Pokemon name."
     if (loading) return "Loading..."
     return (
     <>
         <div className="pokemon-wrapper">
-            <div>id: {data.id}</div>
+            <div className="pokemon-api-id">
+                <div className="pokemon-api-id-title">PokeAPI ID</div>
+                <div className="pokemon-api-id-value">{data.id}</div>
+            </div>
             <div className="pokemon-details-wrapper">
                 <section className="pokemon-details-section">
-                    <div className="pokemon-details-item">
-                        <div className="pokemon-details-item-title">name</div>
-                        <div className="pokemon-details-item-spacer"></div>
-                        <div className="pokemon-details-item-value">{data.name}</div>
-                    </div>
-                    <div className="pokemon-details-item">
-                        <div className="pokemon-details-item-title">base_experience</div>
-                        <div className="pokemon-details-item-spacer"></div>
-                        <div className="pokemon-details-item-value">{data.base_experience}</div>
-                    </div>
-                    <div className="pokemon-details-item">
-                        <div className="pokemon-details-item-title">height</div>
-                        <div className="pokemon-details-item-spacer"></div>
-                        <div className="pokemon-details-item-value">{data.height}</div>
-                    </div>
-                    <div className="pokemon-details-item">
-                        <div className="pokemon-details-item-title">is_default </div>
-                        <div className="pokemon-details-item-spacer"></div>
-                        <div className="pokemon-details-item-value">{data.is_default}</div>
-                    </div>
-                    <div className="pokemon-details-item">
-                        <div className="pokemon-details-item-title">order </div>
-                        <div className="pokemon-details-item-spacer"></div>
-                        <div className="pokemon-details-item-value">{data.order}</div>
-                    </div>
-                    <div className="pokemon-details-item">
-                        <div className="pokemon-details-item-title">weight </div>
-                        <div className="pokemon-details-item-spacer"></div>
-                        <div className="pokemon-details-item-value">{data.weight}</div>
-                    </div>
+                    {DATA_PROPS.map((item, index) => {
+                        return (
+                            <div className="pokemon-details-item" key={index} >
+                                <div className="pokemon-details-item-title">{item.title}</div>
+                                <div className="pokemon-details-item-spacer"></div>
+                                <div className="pokemon-details-item-value">{parseProps(item.prop)}</div>
+                            </div>
+                        )
+                    })}
                 </section>
-                <div>abilites:
-                    <div className="pokemon-detail-list">
-                        {data.abilities.map((item, index)=> {
-                            return (<span key={index}>{item.ability.name} | </span>)
-                        })}
-                    </div>
+
+                <div className="pokemon-details-table">
+                    {DATA_LISTS.map((item, index) => {
+                        return (   
+                            <div className="pokemon-details-table-row" key={index}>
+                                <div className="pokemon-details-table-cell pokemon-details-table-cell-title">{item.list}</div>
+                                <div className="pokemon-details-table-cell pokemon-details-table-cell-value">
+                                    {data[item.list].map((detail, j)=> {
+                                        return (<span key={j}>{detail[item.object].name} | </span>)
+                                    })}
+                                </div>
+                                <div className="pokemon-details-table-cell pokemon-details-table-cell-aside">
+                                    {">>"} 
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
                 {/*         
                 <div>{data.forms}</div>
@@ -59,13 +91,6 @@ export default function Pokemon({data, loading}:PokemonProps) {
                 <div>location_area_encounters: {data.location_area_encounters}</div>
                 <div>{data.held_items}</div>
                 */}
-                <div>moves:
-                    <div className="pokemon-detail-list">
-                        {data.moves.map((item, index)=> {
-                            return (<span key={index}>{item.move.name} | </span>)
-                        })}
-                    </div>
-                </div>
                 <div>species_name: {data.species.name}</div>
                 <div><img src={data.sprites.front_default} /></div>
                 {/*
